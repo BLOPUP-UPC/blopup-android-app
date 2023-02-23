eb7f8d4c78a51793c163773f3330c21bcdfb3422
marta.ortuno <marta.ortuno@thoughtworks.com>
2023-02-23 15:46:47 +0100

    #130 Green test in CreatePatient (happy and uphappy path)
    Authors: Iñaki Pumarega, Jasmin Bower and Marta Ortuño

--- openmrs-client/src/androidTest/java/openmrs/activities/addeditpatient/CreatePatientUITTest.java
+++ openmrs-client/src/androidTest/java/openmrs/activities/addeditpatient/CreatePatientUITTest.java
@@ -8,17 +8,22 @@ import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isChecked;
 import static androidx.test.espresso.matcher.ViewMatchers.isNotChecked;
+import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import java.util.Arrays;
 
 import edu.upc.R;
 import edu.upc.openmrs.activities.addeditpatient.AddEditPatientActivity;
@@ -26,28 +31,22 @@ import edu.upc.openmrs.activities.addeditpatient.AddEditPatientActivity;
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class CreatePatientUITTest {
-    private String firstName, familyName, estimatedYear;
 
     @Rule
-    public ActivityTestRule<AddEditPatientActivity> AddEditPatientActivityTestRule =
+    public ActivityTestRule<AddEditPatientActivity> activityTestRule =
             new ActivityTestRule<>(AddEditPatientActivity.class);
-
-    @Before
-    public void initValidString() {
-        this.firstName = "Queens";
-        this.familyName = "Scarlet";
-        this.estimatedYear = "50";
-    }
 
     @Test
     public void registerPatientWithOnlyMandatoryFieldsSuccessfully() {
+        String firstName = "Queens", familyName = "Scarlet", estimatedYear = "50";
+
         onView(withId(R.id.firstName))
-                .perform(typeText(this.firstName), closeSoftKeyboard())
-                .check(matches(withText(this.firstName)));
+                .perform(typeText(firstName), closeSoftKeyboard())
+                .check(matches(withText(firstName)));
 
         onView(withId(R.id.surname))
-                .perform(typeText(this.familyName), closeSoftKeyboard())
-                .check(matches(withText(this.familyName)));
+                .perform(typeText(familyName), closeSoftKeyboard())
+                .check(matches(withText(familyName)));
 
         onView(withId(R.id.male)).perform(click()).check(matches(isChecked()));
         onView(withId(R.id.female)).check(matches(isNotChecked()));
@@ -55,8 +54,32 @@ public class CreatePatientUITTest {
 
         onView(withId(R.id.addressOne)).perform(scrollTo());
         onView(withId(R.id.estimatedYear))
-                .perform(click(), typeText(this.estimatedYear));
+                .perform(click(), typeText(estimatedYear));
 
         onView(withId(R.id.submitButton)).perform(click());
+    }
+
+    @Test
+    public void registerPatientFailsWhenCreateItWithoutGender() {
+        String firstName = "Juan", familyName = "Macson", estimatedYear = "99";
+
+        onView(withId(R.id.firstName))
+                .perform(typeText(firstName), closeSoftKeyboard())
+                .check(matches(withText(firstName)));
+
+        onView(withId(R.id.surname))
+                .perform(typeText(familyName), closeSoftKeyboard())
+                .check(matches(withText(familyName)));
+
+        onView(withId(R.id.addressOne)).perform(scrollTo());
+        onView(withId(R.id.estimatedYear))
+                .perform(click(), typeText(estimatedYear));
+
+
+        onView(withId(R.id.gendererror))
+                .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.GONE)));
+        onView(withId(R.id.submitButton)).perform(click());
+        onView(withId(R.id.gendererror))
+                .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
     }
 }
