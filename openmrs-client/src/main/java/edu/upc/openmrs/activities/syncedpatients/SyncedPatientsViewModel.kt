95ea38aff7b918fb9a6d03f270ddc8fe98b14c20
Olaolu Ajose <115143085+olaolu-tws@users.noreply.github.com>
2023-04-11 18:26:04 +0200

    #161 Search Local If User If Offline. Author: Olaolu
    Co-Authored-By: carolinaacostaTW <109658607+carolinaacostaTW@users.noreply.github.com>

--- openmrs-client/src/main/java/edu/upc/openmrs/activities/syncedpatients/SyncedPatientsViewModel.kt
+++ openmrs-client/src/main/java/edu/upc/openmrs/activities/syncedpatients/SyncedPatientsViewModel.kt
@@ -1,18 +1,21 @@
 package edu.upc.openmrs.activities.syncedpatients
 
+import dagger.hilt.android.lifecycle.HiltViewModel
+import edu.upc.openmrs.utilities.FilterUtil
+import edu.upc.sdk.library.api.repository.PatientRepository
 import edu.upc.sdk.library.dao.PatientDAO
 import edu.upc.sdk.library.dao.VisitDAO
 import edu.upc.sdk.library.models.OperationType
 import edu.upc.sdk.library.models.Patient
-import dagger.hilt.android.lifecycle.HiltViewModel
-import edu.upc.openmrs.utilities.FilterUtil
+import edu.upc.sdk.utilities.NetworkUtils.isOnline
 import rx.android.schedulers.AndroidSchedulers
 import javax.inject.Inject
 
 @HiltViewModel
 class SyncedPatientsViewModel @Inject constructor(
     private val patientDAO: PatientDAO,
-    private val visitDAO: VisitDAO
+    private val visitDAO: VisitDAO,
+    private val patientRepository: PatientRepository
 ) : edu.upc.openmrs.activities.BaseViewModel<List<Patient>>() {
 
     fun fetchSyncedPatients() {
@@ -27,15 +30,26 @@ class SyncedPatientsViewModel @Inject constructor(
 
     fun fetchSyncedPatients(query: String) {
         setLoading()
-        addSubscription(patientDAO.allPatients
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(
-                { patients: List<Patient> ->
-                    val filteredPatients = FilterUtil.getPatientsFilteredByQuery(patients, query)
-                    setContent(filteredPatients)
-                },
-                { setError(it, OperationType.PatientSearching) }
-            ))
+        if (isOnline()) {
+            addSubscription(patientRepository.findPatients(query)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                    { patientList -> setContent(patientList) },
+                    { setError(it, OperationType.PatientSearching) }
+                )
+            )
+        } else {
+            addSubscription(patientDAO.allPatients
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                    { patients: List<Patient> ->
+                        val filteredPatients =
+                            FilterUtil.getPatientsFilteredByQuery(patients, query)
+                        setContent(filteredPatients)
+                    },
+                    { setError(it, OperationType.PatientSearching) }
+                ))
+        }
     }
 
     fun deleteSyncedPatient(patient: Patient) {
