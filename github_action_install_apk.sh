f72deb71a9a6fdfccf70a64c35310efa6d774fa1
Alejandro Batanero <alejandro.batanero@thoughtworks.com>
2023-09-19 12:57:52 +0200

    #BLOPUP-268 Install apk on emulator

---
+++ github_action_install_apk.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+ADB="/Users/runner/Library/Android/sdk/platform-tools/adb"
+
+echo "Build apk"
+./gradlew :openmrs-client:assembleDebug
+
+echo "Installing APK"
+$ADB install openmrs-client/build/outputs/apk/debug/openmrs-client-debug.apk
+
+echo "Starting maestro"
+maestro test --format=junit --output=report.xml --no-ansi .maestro
