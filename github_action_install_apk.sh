b66d4f4a12aee6f94b5d2d14fb2d50667bce8674
rosapalli <rosa.palli@thoughtworks.com>
2023-09-20 20:44:23 +0200

    #268: increase driver timeout to 60s

--- github_action_install_apk.sh
+++ github_action_install_apk.sh
@@ -9,4 +9,5 @@ echo "Installing APK"
 $ADB install openmrs-client/build/outputs/apk/debug/openmrs-client-debug.apk
 
 echo "Starting maestro"
+export MAESTRO_DRIVER_STARTUP_TIMEOUT=60000 # setting 60 seconds
 maestro record --no-ansi .maestro/e2e-register-patient.yaml
