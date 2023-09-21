c09f734922a6c98cc1329c10689f700a6d9d4d7e
Alejandro Batanero <alejandro.batanero@thoughtworks.com>
2023-09-21 15:54:43 +0200

    #BLOPUP-268 Send env variables to maestro

--- github_action_install_apk.sh
+++ github_action_install_apk.sh
@@ -8,11 +8,13 @@ echo "Build apk"
 echo "Installing APK"
 $ADB install openmrs-client/build/outputs/apk/debug/openmrs-client-debug.apk
 
-echo "Starting screemrecord"
+echo "Starting screenrecord"
 $ADB emu screenrecord start --time-limit 460 maestro.webm
 
+echo "Starting variables de entorno"
+echo $MAESTRO_TEST_PASSWORD
 echo $MAESTRO_TEST_USERNAME
 
 echo "Starting maestro"
 export MAESTRO_DRIVER_STARTUP_TIMEOUT=60000 # setting 60 seconds
-maestro record --no-ansi .maestro/e2e-register-patient.yaml
+maestro record -e USERNAME=testing.maestro -e PASSWORD=$MAESTRO_TEST_PASSWORD --no-ansi .maestro/e2e-register-patient.yaml
