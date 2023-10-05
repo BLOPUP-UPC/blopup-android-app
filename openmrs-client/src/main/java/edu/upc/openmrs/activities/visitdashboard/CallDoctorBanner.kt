81794b13f6aaffcc86a9230be9405306b03e4c99
rosapalli <rosa.palli@thoughtworks.com>
2023-10-05 10:53:15 +0200

    #287: create layout and class for CallDoctorBanner

---
+++ openmrs-client/src/main/java/edu/upc/openmrs/activities/visitdashboard/CallDoctorBanner.kt
@@ -0,0 +1,53 @@
+package edu.upc.openmrs.activities.visitdashboard
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams
+import androidx.fragment.app.DialogFragment
+import edu.upc.R
+import edu.upc.databinding.CallDoctorBannerBinding
+
+class CallDoctorBanner : DialogFragment() {
+    private lateinit var callDoctorBinding: CallDoctorBannerBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        callDoctorBinding = CallDoctorBannerBinding.inflate(inflater, container, false)
+        return callDoctorBinding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        setListeners()
+        formatDialog()
+    }
+
+    override fun getTheme(): Int = R.style.NoMarginDialog
+
+    private fun formatDialog() {
+        val width = LayoutParams.MATCH_PARENT
+        val height = LayoutParams.WRAP_CONTENT
+
+        dialog!!.window!!.setLayout(width,height)
+        dialog!!.window!!.setGravity(Gravity.TOP)
+
+    }
+
+    private fun setListeners() {
+        callDoctorBinding.callButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_CALL)
+            intent.data = Uri.parse("tel:+34621039238")
+            startActivity(intent)
+        }
+        callDoctorBinding.cancelButton.setOnClickListener { dismiss() }
+    }
+}
+
