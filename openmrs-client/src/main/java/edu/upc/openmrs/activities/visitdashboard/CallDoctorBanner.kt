652843ef066e7c8abb94433491177c6aa0938c3d
Alejandro Batanero <alejandro.batanero@thoughtworks.com>
2023-11-17 10:03:22 +0100

    Remove dangerous permissions SMS_SEND

--- openmrs-client/src/main/java/edu/upc/openmrs/activities/visitdashboard/CallDoctorBanner.kt
+++ openmrs-client/src/main/java/edu/upc/openmrs/activities/visitdashboard/CallDoctorBanner.kt
@@ -7,8 +7,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import edu.upc.BuildConfig
-import edu.upc.Secrets
 import edu.upc.databinding.CallDoctorBannerBinding
 import edu.upc.openmrs.utilities.SecretsUtils
 import edu.upc.openmrs.utilities.makeGone
