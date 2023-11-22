5b7019d3e00cf149f02d5b306abe8714b7dd8692
Carolina Acosta <carolina.acosta@thoughtworks.com>
2023-11-22 12:58:31 -0300

    #BLOPUP-297 - Add tests to NavigationBarActivity

--- openmrs-client/src/main/java/edu/upc/openmrs/activities/NavigationBarActivity.kt
+++ openmrs-client/src/main/java/edu/upc/openmrs/activities/NavigationBarActivity.kt
@@ -1,6 +1,5 @@
 package edu.upc.openmrs.activities
 
-import android.app.Activity
 import android.content.Intent
 import android.view.MenuItem
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -16,7 +15,7 @@ open class NavigationBarActivity : ACBaseActivity() {
         }
     }
 
-    protected fun launchSelectedActivity(item: MenuItem): Boolean {
+    fun launchSelectedActivity(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.home_screen -> { openActivity(DashboardActivity::class.java) }
             R.id.search_patients -> { openActivity(SyncedPatientsActivity::class.java) }
@@ -25,7 +24,7 @@ open class NavigationBarActivity : ACBaseActivity() {
         return true
     }
 
-    private fun openActivity(activity: Class<*>) {
+    fun openActivity(activity: Class<*>) {
         val intent = Intent(this, activity)
         startActivity(intent)
         overridePendingTransition(0, 0)
