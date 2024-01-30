c4394c000bd3ba4011afc1e9b4aae73b65c455bd
rosapalli <rosa.palli@thoughtworks.com>
2024-01-30 15:03:48 +0100

    BLOPUP-355: report to crashlytics when messaging doctor fails

--- openmrs-client/src/main/java/edu/upc/sdk/library/api/repository/DoctorRepository.kt
+++ openmrs-client/src/main/java/edu/upc/sdk/library/api/repository/DoctorRepository.kt
@@ -2,13 +2,15 @@ package edu.upc.sdk.library.api.repository
 
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
+import edu.upc.sdk.library.CrashlyticsLoggerImpl
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import java.net.UnknownHostException
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class DoctorRepository @Inject constructor() : BaseRepository(null) {
+class DoctorRepository @Inject constructor() : BaseRepository(CrashlyticsLoggerImpl()) {
     suspend fun sendMessageToDoctor(message: String): Result<Boolean> =
         withContext(Dispatchers.IO) {
             val contactRequest = ContactDoctorRequest(DOCTOR_PROVIDER_UUID, message)
@@ -16,11 +18,14 @@ class DoctorRepository @Inject constructor() : BaseRepository(null) {
             try {
                 val response = restApi.contactDoctor(contactRequest).execute()
                 if (!response.isSuccessful) {
+                    crashlytics.reportUnsuccessfulResponse(response, FAILED_MESSAGE_DOCTOR)
                     return@withContext Result.failure(
-                        Exception("Error sending message to doctor: ${response.errorBody()?.string()}"))
+                        Exception("$FAILED_MESSAGE_DOCTOR: ${response.errorBody()?.string()}")
+                    )
                 }
                 return@withContext Result.success(true)
             } catch (e: Exception) {
+                crashlytics.reportException(e, FAILED_MESSAGE_DOCTOR)
                 return@withContext Result.failure(e)
             }
         }
@@ -28,6 +33,7 @@ class DoctorRepository @Inject constructor() : BaseRepository(null) {
 
     companion object {
         const val DOCTOR_PROVIDER_UUID = "2775ad68-1a28-450f-a270-6ac5d0120636"
+        private const val FAILED_MESSAGE_DOCTOR = "Failed to message doctor"
     }
 
     data class ContactDoctorRequest(
