91e315cd8c8ef624836fd35c69d388f4c1a2245b
Alejandro Batanero <alejandro.batanero@thoughtworks.com>
2024-02-09 15:17:22 +0100

    BLOPUP-369 Move Blood pressure chart to class

---
+++ openmrs-client/src/main/java/edu/upc/openmrs/activities/patientdashboard/charts/BloodPressureChart.kt
@@ -0,0 +1,246 @@
+package edu.upc.openmrs.activities.patientdashboard.charts
+
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.Drawable
+import androidx.core.content.ContextCompat
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.LimitLine
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.listener.OnChartGestureListener
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener
+import edu.upc.R
+import edu.upc.blopup.toggles.check
+import edu.upc.blopup.toggles.treatmentChartToggle
+import java.time.LocalDate
+
+class BloodPressureChart(private val context: Context) {
+
+    private val maxValuesInView = 3f
+    private val maxBloodPressureValueShown = 200f
+    private val minBloodPressureValueShown = 40f
+
+    fun paintBloodPressureChart(chartLayout: LineChart, bloodPressureData: List<BloodPressureChartValue>) {
+        setBloodPressureData(chartLayout, bloodPressureData)
+        setChartMaxAndMinimumLimitLines(chartLayout)
+        applyCommonChartStyles(chartLayout)
+        applyBloodPressureChartStyles(chartLayout)
+    }
+
+    fun paintTreatmentsChart(chartLayout: LineChart, bloodPressureData: List<BloodPressureChartValue>) {
+        setTreatmentsData(chartLayout, getTreatmentValues(bloodPressureData))
+        applyCommonChartStyles(chartLayout)
+        applyTreatmentsChartStyles(chartLayout)
+    }
+
+    private fun setBloodPressureData(
+        chart: LineChart,
+        bloodPressureData: List<BloodPressureChartValue>
+    ) {
+        val allDatesValues = bloodPressureData.map { it.date.toString() }
+
+        // Create entries where x is 0 to n-1 and y is the value
+        // Then we show the date in the x axis based on the order
+        val systolicEntries = bloodPressureData.mapIndexed { index, value ->
+            Entry(
+                index.toFloat(),
+                value.systolic,
+                getSystolicPointIcon(value.systolic)
+            )
+        }
+        val diastolicEntries = bloodPressureData.mapIndexed { index, value ->
+            Entry(
+                index.toFloat(),
+                value.diastolic,
+                getDiastolicPointIcon(value.diastolic)
+            )
+        }
+
+        val systolicLine = LineDataSet(systolicEntries, "")
+        val diastolicLine = LineDataSet(diastolicEntries, "")
+
+        //makes the line between data points transparent
+        diastolicLine.color = Color.TRANSPARENT
+        systolicLine.color = Color.TRANSPARENT
+
+        // Size of the vale text (systolic or diastolic)
+        diastolicLine.valueTextSize = 12f
+        systolicLine.valueTextSize = 12f
+
+        // We set the circle radius to 8 to put the text value above the actual icon
+        // This size must be adjusted if the icon size changes
+        systolicLine.circleRadius = 8F
+        diastolicLine.circleRadius = 8F
+
+        // We remove the native circle as we are showing a custom icon (red or green point)
+        diastolicLine.setCircleColor(Color.TRANSPARENT)
+        systolicLine.setCircleColor(Color.TRANSPARENT)
+
+        chart.data = LineData(listOf(systolicLine, diastolicLine))
+        // Format the values in the x axis to show the date instead of 0 to n-1
+        chart.xAxis.valueFormatter = MyValueFormatter(allDatesValues)
+    }
+
+    private fun setChartMaxAndMinimumLimitLines(chart: LineChart) {
+        val minLimitSystolic = generateDashedLineAt(80f, Color.GREEN)
+        val maxLimitSystolic = generateDashedLineAt(90f, Color.RED)
+
+        val minLimitDiastolic = generateDashedLineAt(130f, Color.GREEN)
+        val maxLimitDiastolic = generateDashedLineAt(140f, Color.RED)
+
+        chart.axisLeft.addLimitLine(minLimitSystolic)
+        chart.axisLeft.addLimitLine(maxLimitSystolic)
+        chart.axisLeft.addLimitLine(minLimitDiastolic)
+        chart.axisLeft.addLimitLine(maxLimitDiastolic)
+    }
+
+    private fun applyCommonChartStyles(mChart: LineChart) {
+        mChart.description.isEnabled = false
+        // to make it scrollable
+        mChart.setVisibleXRangeMaximum(maxValuesInView)
+        // to remove values in right side of screen
+        mChart.axisRight.isEnabled = false
+        //to open the chart focused on the most recent values
+        mChart.moveViewToX(mChart.xAxis.axisMaximum)
+        mChart.legend.isEnabled = false
+        //to add some space before the first and last values on the chart
+        mChart.xAxis.axisMinimum = -0.1F
+        mChart.xAxis.axisMaximum = mChart.xAxis.axisMaximum + 0.1F
+
+        // To show only
+        mChart.xAxis.granularity = 1f
+
+        //min values in the axis with the values
+        mChart.axisLeft.axisMinimum = minBloodPressureValueShown
+        mChart.isDoubleTapToZoomEnabled = false
+        mChart.isScaleYEnabled = false
+        // TODO: Enable if we are able to sync pinch zoom
+        mChart.isScaleXEnabled = false
+    }
+
+    private fun applyBloodPressureChartStyles(mChart: LineChart) {
+        treatmentChartToggle.check({
+            mChart.setTouchEnabled(false)
+        }, {
+            mChart.setTouchEnabled(true)
+        })
+        //to display the dates only in the bottom and not in the top as well
+        mChart.xAxis.position = XAxis.XAxisPosition.BOTTOM
+        mChart.xAxis.axisLineWidth = 2f
+        mChart.xAxis.axisLineColor = Color.BLACK
+        mChart.axisLeft.axisLineWidth = 2f
+        mChart.axisLeft.axisLineColor = Color.BLACK
+        //max and min values in the axis with the values
+        mChart.axisLeft.axisMaximum = maxBloodPressureValueShown
+
+        // Remove horizontal grid lines
+        mChart.axisLeft.gridColor = Color.TRANSPARENT
+    }
+
+    private fun getTreatmentValues(bloodPressureValues: List<BloodPressureChartValue>): List<TreatmentChartValue> {
+        return bloodPressureValues.map {
+            TreatmentChartValue(
+                it.date,
+                getAdherenceForDate(it.date)
+            )
+        }
+    }
+
+    private fun getAdherenceForDate(date: LocalDate): FollowTreatments {
+        // TODO: Implement logic to get the adherence for a given date
+        return FollowTreatments.entries.shuffled().first()
+    }
+
+    private fun setTreatmentsData(chart: LineChart, treatmentsData: List<TreatmentChartValue>) {
+        val allDatesValues = treatmentsData.map { it.date.toString() }
+        val treatmentsEntries = treatmentsData.mapIndexed { index, value ->
+            Entry(
+                index.toFloat(),
+                230f,
+                getPillIconForAdherence(value.followTreatments)
+            )
+        }
+        val dataSetTreatments = LineDataSet(treatmentsEntries, "")
+
+        // Hide the native circle and value text, we only show the pill icon
+        dataSetTreatments.valueTextSize = 0f
+        dataSetTreatments.color = Color.TRANSPARENT
+        dataSetTreatments.setCircleColor(Color.TRANSPARENT)
+
+        chart.data = LineData(listOf(dataSetTreatments))
+        chart.xAxis.valueFormatter = MyValueFormatter(allDatesValues)
+    }
+
+    private fun getPillIconForAdherence(adherence: FollowTreatments) = when (adherence) {
+        FollowTreatments.NO_TREATMENTS -> ContextCompat.getDrawable(
+            context,
+            R.drawable.ic_treatment_pill
+        )!!.apply { setTint(Color.GRAY) }
+
+        FollowTreatments.FOLLOW_ALL -> ContextCompat.getDrawable(
+            context,
+            R.drawable.ic_treatment_pill
+        )!!.apply {
+            setTint(getColorFromResource(R.color.bp_normal))
+        }
+
+        FollowTreatments.FOLLOW_SOME -> ContextCompat.getDrawable(
+            context,
+            R.drawable.ic_treatment_pill
+        )!!.apply {
+            setTint(getColorFromResource(R.color.bp_ht_stage_I))
+        }
+
+        FollowTreatments.FOLLOW_NONE -> ContextCompat.getDrawable(
+            context,
+            R.drawable.ic_treatment_pill
+        )!!.apply {
+            setTint(getColorFromResource(R.color.bp_ht_stage_II_C))
+        }
+    }
+
+    private fun applyTreatmentsChartStyles(mChart: LineChart) {
+        mChart.setTouchEnabled(true)
+        mChart.xAxis.axisLineColor = Color.TRANSPARENT
+        mChart.xAxis.textColor = Color.TRANSPARENT
+        // Hide vertical grid lines
+        mChart.xAxis.gridColor = Color.TRANSPARENT
+
+        // We want to show the pill above any possible value
+        mChart.axisLeft.axisMaximum = maxBloodPressureValueShown + 40f
+        // Remove horizontal grid lines
+        mChart.axisLeft.gridColor = Color.TRANSPARENT
+        // Hide left axis
+        mChart.axisLeft.textColor = Color.TRANSPARENT
+        mChart.axisLeft.axisLineColor = Color.TRANSPARENT
+    }
+
+    private fun getColorFromResource(resource: Int) =
+        ContextCompat.getColor(context, resource)
+
+
+    private fun generateDashedLineAt(value: Float, color: Int) =
+        LimitLine(value).apply { lineColor = color; lineWidth = 2f; enableDashedLine(10f, 10f, 0f) }
+
+
+    private fun isSystolicHigh(value: Float) = value >= 130F
+    private fun isDiastolicHigh(value: Float) = value >= 80F
+    private fun getSystolicPointIcon(value: Float) = getPointIcon(isSystolicHigh(value))
+    private fun getDiastolicPointIcon(value: Float) = getPointIcon(isDiastolicHigh(value))
+    private fun getPointIcon(isAboveMax: Boolean): Drawable {
+        return if (isAboveMax) {
+            ContextCompat.getDrawable(context, R.drawable.red)!!
+        } else {
+            ContextCompat.getDrawable(context, R.drawable.green)!!
+        }
+    }
+
+    fun setListeners(chart: LineChart, gestureListener: OnChartGestureListener, valueSelectedListener: OnChartValueSelectedListener) {
+        // Listener to sync the two charts scroll
+        chart.onChartGestureListener = gestureListener
+        chart.setOnChartValueSelectedListener(valueSelectedListener)
+    }
+}
--- openmrs-client/src/main/java/edu/upc/openmrs/activities/patientdashboard/charts/ChartsViewActivity.kt
+++ openmrs-client/src/main/java/edu/upc/openmrs/activities/patientdashboard/charts/ChartsViewActivity.kt
@@ -1,25 +1,16 @@
 package edu.upc.openmrs.activities.patientdashboard.charts
 
 import android.content.Context
-import android.graphics.Color
-import android.graphics.drawable.Drawable
-import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.MotionEvent
 import android.widget.ExpandableListView
 import android.widget.Toast
 import androidx.activity.viewModels
-import androidx.annotation.RequiresApi
 import androidx.appcompat.widget.Toolbar
-import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
 import com.github.mikephil.charting.charts.LineChart
-import com.github.mikephil.charting.components.LimitLine
-import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.Entry
-import com.github.mikephil.charting.data.LineData
-import com.github.mikephil.charting.data.LineDataSet
 import com.github.mikephil.charting.highlight.Highlight
 import com.github.mikephil.charting.listener.ChartTouchListener
 import com.github.mikephil.charting.listener.OnChartGestureListener
@@ -38,30 +29,21 @@ import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
 
-@RequiresApi(Build.VERSION_CODES.O)
 class ChartsViewActivity : ACBaseActivity(), OnChartGestureListener, OnChartValueSelectedListener {
 
     private val viewModel: ChartsViewViewModel by viewModels()
 
-    private lateinit var bloodPressureChart: LineChart
-    private lateinit var treatmentsChart: LineChart
-    private val maxValuesInView = 3f
+    private lateinit var bloodPressureChartView: LineChart
+    private lateinit var treatmentsChartView: LineChart
+    private lateinit var bloodPressureChartPainter: BloodPressureChart
 
     private lateinit var expandableListView: ExpandableListView
     private lateinit var expandableListAdapter: TreatmentsListExpandableListAdapter
     private lateinit var expandableListTitle: List<String>
     private lateinit var expandableListDetail: Map<String, List<TreatmentAdherence>>
 
-    private val maxBloodPressureValueShown = 200f
-    private val minBloodPressureValueShown = 40f
-
     private val patientLocalDbId by lazy {
         this.intent.getBundleExtra(ApplicationConstants.BUNDLE)!!.getInt(PATIENT_ID)
-    }
-
-    companion object {
-        const val BLOOD_PRESSURE = "bloodPressure"
-        const val PATIENT_ID = "patientId"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -71,26 +53,23 @@ class ChartsViewActivity : ACBaseActivity(), OnChartGestureListener, OnChartValu
 
         setToolbar()
 
-        bloodPressureChart = mBinding.bloodPressureChart
-        treatmentsChart = mBinding.treatmentsChart
+        bloodPressureChartView = mBinding.bloodPressureChart
+        treatmentsChartView = mBinding.treatmentsChart
+        bloodPressureChartPainter = BloodPressureChart(this)
 
         val bloodPressureData = getBloodPressureValues()
 
         treatmentChartToggle.check({
-            bloodPressureChart = mBinding.bloodPressureChartWithMargin
-            bloodPressureChart.makeVisible()
+            bloodPressureChartView = mBinding.bloodPressureChartWithMargin
+            bloodPressureChartView.makeVisible()
             mBinding.bloodPressureChart.makeGone()
-            treatmentsChart.makeVisible()
+            treatmentsChartView.makeVisible()
             mBinding.expandableListView.makeVisible()
         })
 
-        setBloodPressureData(bloodPressureChart, bloodPressureData)
-        setTreatmentsData(treatmentsChart, getTreatmentValues(bloodPressureData))
-        setChartMaxAndMinimumLimitLines(bloodPressureChart)
-        applyCommonChartStyles(bloodPressureChart)
-        applyCommonChartStyles(treatmentsChart)
-        applyBloodPressureChartStyles(bloodPressureChart)
-        applyTreatmentsChartStyles(treatmentsChart)
+        bloodPressureChartPainter.paintBloodPressureChart(bloodPressureChartView, bloodPressureData)
+        bloodPressureChartPainter.paintTreatmentsChart(treatmentsChartView, bloodPressureData)
+        bloodPressureChartPainter.setListeners(treatmentsChartView, this, this)
 
         expandableListView = mBinding.expandableListView
 
@@ -135,54 +114,6 @@ class ChartsViewActivity : ACBaseActivity(), OnChartGestureListener, OnChartValu
         }
     }
 
-    private fun setBloodPressureData(
-        chart: LineChart,
-        bloodPressureData: List<BloodPressureChartValue>
-    ) {
-        val allDatesValues = bloodPressureData.map { it.date.toString() }
-
-        // Create entries where x is 0 to n-1 and y is the value
-        // Then we show the date in the x axis based on the order
-        val systolicEntries = bloodPressureData.mapIndexed { index, value ->
-            Entry(
-                index.toFloat(),
-                value.systolic,
-                getSystolicPointIcon(value.systolic)
-            )
-        }
-        val diastolicEntries = bloodPressureData.mapIndexed { index, value ->
-            Entry(
-                index.toFloat(),
-                value.diastolic,
-                getDiastolicPointIcon(value.diastolic)
-            )
-        }
-
-        val systolicLine = LineDataSet(systolicEntries, "")
-        val diastolicLine = LineDataSet(diastolicEntries, "")
-
-        //makes the line between data points transparent
-        diastolicLine.color = Color.TRANSPARENT
-        systolicLine.color = Color.TRANSPARENT
-
-        // Size of the vale text (systolic or diastolic)
-        diastolicLine.valueTextSize = 12f
-        systolicLine.valueTextSize = 12f
-
-        // We set the circle radius to 8 to put the text value above the actual icon
-        // This size must be adjusted if the icon size changes
-        systolicLine.circleRadius = 8F
-        diastolicLine.circleRadius = 8F
-
-        // We remove the native circle as we are showing a custom icon (red or green point)
-        diastolicLine.setCircleColor(Color.TRANSPARENT)
-        systolicLine.setCircleColor(Color.TRANSPARENT)
-
-        chart.data = LineData(listOf(systolicLine, diastolicLine))
-        // Format the values in the x axis to show the date instead of 0 to n-1
-        chart.xAxis.valueFormatter = MyValueFormatter(allDatesValues)
-    }
-
     private fun getBloodPressureValues(): List<BloodPressureChartValue> {
         val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
 
@@ -201,163 +132,8 @@ class ChartsViewActivity : ACBaseActivity(), OnChartGestureListener, OnChartValu
         return chartValues.sortedBy { it.date }.distinctBy { it.date }
     }
 
-    private fun getTreatmentValues(bloodPressureValues: List<BloodPressureChartValue>): List<TreatmentChartValue> {
-        return bloodPressureValues.map {
-            TreatmentChartValue(
-                it.date,
-                getAdherenceForDate(it.date)
-            )
-        }
-    }
-
-    private fun getAdherenceForDate(date: LocalDate): FollowTreatments {
-        // TODO: Implement logic to get the adherence for a given date
-        return FollowTreatments.values().toList().shuffled().first()
-    }
-
-    private fun getPillIconForAdherence(adherence: FollowTreatments) = when (adherence) {
-        FollowTreatments.NO_TREATMENTS -> ContextCompat.getDrawable(
-            applicationContext,
-            R.drawable.ic_treatment_pill
-        )!!.apply { setTint(Color.GRAY) }
-
-        FollowTreatments.FOLLOW_ALL -> ContextCompat.getDrawable(
-            applicationContext,
-            R.drawable.ic_treatment_pill
-        )!!.apply {
-            setTint(getColorFromResource(R.color.bp_normal))
-        }
-
-        FollowTreatments.FOLLOW_SOME -> ContextCompat.getDrawable(
-            applicationContext,
-            R.drawable.ic_treatment_pill
-        )!!.apply {
-            setTint(getColorFromResource(R.color.bp_ht_stage_I))
-        }
-
-        FollowTreatments.FOLLOW_NONE -> ContextCompat.getDrawable(
-            applicationContext,
-            R.drawable.ic_treatment_pill
-        )!!.apply {
-            setTint(getColorFromResource(R.color.bp_ht_stage_II_C))
-        }
-    }
-
-    private fun getColorFromResource(resource: Int) =
-        ContextCompat.getColor(applicationContext, resource)
-
-    private fun setTreatmentsData(chart: LineChart, treatmentsData: List<TreatmentChartValue>) {
-        val allDatesValues = treatmentsData.map { it.date.toString() }
-        val treatmentsEntries = treatmentsData.mapIndexed { index, value ->
-            Entry(
-                index.toFloat(),
-                230f,
-                getPillIconForAdherence(value.followTreatments)
-            )
-        }
-        val dataSetTreatments = LineDataSet(treatmentsEntries, "")
-
-        // Hide the native circle and value text, we only show the pill icon
-        dataSetTreatments.valueTextSize = 0f
-        dataSetTreatments.color = Color.TRANSPARENT
-        dataSetTreatments.setCircleColor(Color.TRANSPARENT)
-
-        chart.data = LineData(listOf(dataSetTreatments))
-        chart.xAxis.valueFormatter = MyValueFormatter(allDatesValues)
-        // Listener to sync the two charts scroll
-        chart.onChartGestureListener = this
-        chart.setOnChartValueSelectedListener(this)
-    }
-
-    private fun setChartMaxAndMinimumLimitLines(chart: LineChart) {
-        val minLimitSystolic = generateDashedLineAt(80f, Color.GREEN)
-        val maxLimitSystolic = generateDashedLineAt(90f, Color.RED)
-
-        val minLimitDiastolic = generateDashedLineAt(130f, Color.GREEN)
-        val maxLimitDiastolic = generateDashedLineAt(140f, Color.RED)
-
-        chart.axisLeft.addLimitLine(minLimitSystolic)
-        chart.axisLeft.addLimitLine(maxLimitSystolic)
-        chart.axisLeft.addLimitLine(minLimitDiastolic)
-        chart.axisLeft.addLimitLine(maxLimitDiastolic)
-    }
-
-    private fun generateDashedLineAt(value: Float, color: Int) =
-        LimitLine(value).apply { lineColor = color; lineWidth = 2f; enableDashedLine(10f, 10f, 0f) }
-
-    private fun applyCommonChartStyles(mChart: LineChart) {
-        mChart.description.isEnabled = false
-        // to make it scrollable
-        mChart.setVisibleXRangeMaximum(maxValuesInView)
-        // to remove values in right side of screen
-        mChart.axisRight.isEnabled = false
-        //to open the chart focused on the most recent values
-        mChart.moveViewToX(mChart.xAxis.axisMaximum)
-        mChart.legend.isEnabled = false
-        //to add some space before the first and last values on the chart
-        mChart.xAxis.axisMinimum = -0.1F
-        mChart.xAxis.axisMaximum = mChart.xAxis.axisMaximum + 0.1F
-
-        // To show only
-        mChart.xAxis.granularity = 1f
-
-        //min values in the axis with the values
-        mChart.axisLeft.axisMinimum = minBloodPressureValueShown
-        mChart.isDoubleTapToZoomEnabled = false
-        mChart.isScaleYEnabled = false
-        // TODO: Enable if we are able to sync pinch zoom
-        mChart.isScaleXEnabled = false
-    }
-
-    private fun applyBloodPressureChartStyles(mChart: LineChart) {
-        treatmentChartToggle.check({
-            mChart.setTouchEnabled(false)
-        }, {
-            mChart.setTouchEnabled(true)
-        })
-        //to display the dates only in the bottom and not in the top as well
-        mChart.xAxis.position = XAxis.XAxisPosition.BOTTOM
-        mChart.xAxis.axisLineWidth = 2f
-        mChart.xAxis.axisLineColor = Color.BLACK
-        mChart.axisLeft.axisLineWidth = 2f
-        mChart.axisLeft.axisLineColor = Color.BLACK
-        //max and min values in the axis with the values
-        mChart.axisLeft.axisMaximum = maxBloodPressureValueShown
-
-        // Remove horizontal grid lines
-        mChart.axisLeft.gridColor = Color.TRANSPARENT
-    }
-
-    private fun applyTreatmentsChartStyles(mChart: LineChart) {
-        mChart.setTouchEnabled(true)
-        mChart.xAxis.axisLineColor = Color.TRANSPARENT
-        mChart.xAxis.textColor = Color.TRANSPARENT
-        // Hide vertical grid lines
-        mChart.xAxis.gridColor = Color.TRANSPARENT
-
-        // We want to show the pill above any possible value
-        mChart.axisLeft.axisMaximum = maxBloodPressureValueShown + 40f
-        // Remove horizontal grid lines
-        mChart.axisLeft.gridColor = Color.TRANSPARENT
-        // Hide left axis
-        mChart.axisLeft.textColor = Color.TRANSPARENT
-        mChart.axisLeft.axisLineColor = Color.TRANSPARENT
-    }
-
-    private fun isSystolicHigh(value: Float) = value >= 130F
-    private fun isDiastolicHigh(value: Float) = value >= 80F
-    private fun getSystolicPointIcon(value: Float) = getPointIcon(isSystolicHigh(value))
-    private fun getDiastolicPointIcon(value: Float) = getPointIcon(isDiastolicHigh(value))
-    private fun getPointIcon(isAboveMax: Boolean): Drawable {
-        return if (isAboveMax) {
-            ContextCompat.getDrawable(applicationContext, R.drawable.red)!!
-        } else {
-            ContextCompat.getDrawable(applicationContext, R.drawable.green)!!
-        }
-    }
-
     override fun onChartTranslate(me: MotionEvent?, dX: Float, dY: Float) {
-        bloodPressureChart.moveViewToX(treatmentsChart.lowestVisibleX)
+        bloodPressureChartView.moveViewToX(treatmentsChartView.lowestVisibleX)
     }
 
     override fun onChartGestureStart(
@@ -406,6 +182,11 @@ class ChartsViewActivity : ACBaseActivity(), OnChartGestureListener, OnChartValu
     }
 
     override fun onNothingSelected() {
+    }
+
+    companion object {
+        const val BLOOD_PRESSURE = "bloodPressure"
+        const val PATIENT_ID = "patientId"
     }
 }
 
