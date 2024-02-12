99ef7ef73675d775d9278e72481084d1fc2536cf
rosapalli <rosa.palli@thoughtworks.com>
2024-02-12 11:57:34 +0100

    Add receiver exported flag when registering receiver in ACBaseActivity for sdk Tiramisu and up

--- openmrs-client/src/main/java/edu/upc/openmrs/activities/ACBaseActivity.java
+++ openmrs-client/src/main/java/edu/upc/openmrs/activities/ACBaseActivity.java
@@ -20,6 +20,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -97,7 +98,11 @@ public abstract class ACBaseActivity extends AppCompatActivity {
                 && !(this instanceof ContactUsActivity) && !(this instanceof SplashActivity)) {
             mAuthorizationManager.moveToLoginActivity();
         }
-        registerReceiver(mPasswordChangedReceiver, mIntentFilter);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            registerReceiver(mPasswordChangedReceiver, mIntentFilter, RECEIVER_EXPORTED);
+        } else {
+            registerReceiver(mPasswordChangedReceiver,mIntentFilter);
+        }
         ToastUtil.setAppVisible(true);
     }
 
