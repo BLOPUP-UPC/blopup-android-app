c1702c7cb069abfdb5fa7cfb82cec6f37351a5f2
Carolina Acosta <carolina.acosta@thoughtworks.com>
2024-03-05 09:41:01 +0100

    382 - Add treatment to UI test and change test name class.

--- openmrs-client/src/test/java/edu/upc/blopup/ui/takingvitals/TreatmentAdherenceTest.kt
+++ openmrs-client/src/test/java/edu/upc/blopup/ui/takingvitals/TreatmentAdherenceTest.kt
@@ -6,13 +6,13 @@ import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import edu.upc.blopup.ui.takingvitals.screens.TreatmentAdherence
-import edu.upc.sdk.library.models.Treatment
+import edu.upc.sdk.library.models.TreatmentExample
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class VitalsActivityTest {
+class TreatmentAdherenceTest {
 
     @get:Rule
     val composeTestRule = createComposeRule()
@@ -27,18 +27,20 @@ class VitalsActivityTest {
         composeTestRule.onNodeWithText("Treatments could not be loaded.").assertIsDisplayed()
         composeTestRule.onNodeWithText("Try again").assertIsDisplayed()
         composeTestRule.onNodeWithText("Is the patient following these treatments?").assertIsNotDisplayed()
-
     }
 
     @Test
     fun `if fetch treatment is success, it should ask if patient is following those treatments`() {
+        val treatment = TreatmentExample.activeTreatment()
+
         composeTestRule.setContent {
-            TreatmentAdherence(treatments = ResultUiState.Success<List<Treatment>>(emptyList()), createVisit = {}, treatmentAdherence = {}) {
+            TreatmentAdherence(treatments = ResultUiState.Success(listOf(treatment)), createVisit = {}, treatmentAdherence = {}) {
             }
         }
 
         composeTestRule.onNodeWithText("Is the patient following these treatments?").assertIsDisplayed()
         composeTestRule.onNodeWithText("Treatments could not be loaded.").assertIsNotDisplayed()
+        composeTestRule.onNodeWithText(treatment.medicationName).assertIsDisplayed()
 
     }
 }
