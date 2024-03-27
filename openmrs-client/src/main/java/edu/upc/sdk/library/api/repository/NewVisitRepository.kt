66127850c536dfe1d9ac7df8421faf287412d242
rosapalli <rosa.palli@thoughtworks.com>
2024-03-27 10:27:42 +0100

    BLOPUP-385: implement end visit method in new visit repository

--- openmrs-client/src/main/java/edu/upc/sdk/library/api/repository/NewVisitRepository.kt
+++ openmrs-client/src/main/java/edu/upc/sdk/library/api/repository/NewVisitRepository.kt
@@ -2,15 +2,22 @@ package edu.upc.sdk.library.api.repository
 
 import edu.upc.blopup.model.BloodPressure
 import edu.upc.blopup.model.Visit
+import edu.upc.sdk.library.OpenMRSLogger
 import edu.upc.sdk.library.api.RestApi
+import edu.upc.sdk.library.dao.VisitDAO
 import edu.upc.sdk.library.models.EncounterType
 import edu.upc.sdk.utilities.DateUtils
+import java.time.LocalDateTime
+import java.time.ZoneOffset
 import java.util.UUID
 import javax.inject.Inject
 import javax.inject.Singleton
+import edu.upc.sdk.library.models.Visit as OpenMRSVisit
 
 @Singleton
-class NewVisitRepository @Inject constructor(val restApi: RestApi) {
+class NewVisitRepository @Inject constructor(val restApi: RestApi, val visitDAO: VisitDAO) {
+
+    private val logger = OpenMRSLogger()
 
     fun getVisitByUuid(visitUuid: UUID): Visit {
 
@@ -20,18 +27,22 @@ class NewVisitRepository @Inject constructor(val restApi: RestApi) {
             openMRSVisit?.encounters?.find { it.encounterType?.display == EncounterType.VITALS }?.observations
 
         val systolic =
-            vitalsObservations?.find { it.display?.contains("Systolic") == true }?.displayValue?.toDouble()?.toInt()
+            vitalsObservations?.find { it.display?.contains("Systolic") == true }?.displayValue?.toDouble()
+                ?.toInt()
                 ?: throw Exception("Systolic is null")
         val diastolic =
-            vitalsObservations.find {  it.display?.contains("Diastolic") == true }?.displayValue?.toDouble()?.toInt()
+            vitalsObservations.find { it.display?.contains("Diastolic") == true }?.displayValue?.toDouble()
+                ?.toInt()
                 ?: throw Exception("Diastolic is null")
         val pulse =
-            vitalsObservations.find {  it.display?.contains("Pulse") == true }?.displayValue?.toDouble()?.toInt()
+            vitalsObservations.find { it.display?.contains("Pulse") == true }?.displayValue?.toDouble()
+                ?.toInt()
                 ?: throw Exception("Pulse is null")
         val weight =
-            vitalsObservations.find {  it.display?.contains("Weight") == true }?.displayValue?.toFloat()
+            vitalsObservations.find { it.display?.contains("Weight") == true }?.displayValue?.toFloat()
         val height =
-            vitalsObservations.find {  it.display?.contains("Height") == true }?.displayValue?.toDouble()?.toInt()
+            vitalsObservations.find { it.display?.contains("Height") == true }?.displayValue?.toDouble()
+                ?.toInt()
 
         val visitStartDate =
             openMRSVisit.startDatetime.let { DateUtils.parseLocalDateFromOpenmrsDate(it) }
@@ -46,8 +57,28 @@ class NewVisitRepository @Inject constructor(val restApi: RestApi) {
             weight
         )
     }
+
+    suspend fun endVisit(visitId: UUID): Boolean {
+        val endVisitDateTime = LocalDateTime.now()
+        val visitWithEndDate = OpenMRSVisit().apply {
+            stopDatetime = endVisitDateTime.toInstant(ZoneOffset.UTC).toString()
+        }
+        val response = restApi.endVisitByUUID(visitId.toString(), visitWithEndDate).execute()
+
+        if (response.isSuccessful) {
+            try {
+                val localVisitId =
+                    visitDAO.getVisitsIDByUUID(visitId.toString()).single().toBlocking().first()
+                val localVisit = visitDAO.getVisitByID(localVisitId).single().toBlocking().first()
+                localVisit.patient.id?.let {
+                    visitDAO.saveOrUpdate(localVisit, it).single().toBlocking().first()
+                }
+            } catch (e: Exception) {
+                logger.e("Error updating visit end date in local database: ${e.javaClass.simpleName}: ${e.message}")
+            }
+            return true
+        } else {
+            throw Exception("endVisitByUuid error: " + response.message())
+        }
+    }
 }
-
-
-
-
--- openmrs-client/src/test/java/edu/upc/sdk/library/api/repository/NewVisitRepositoryTest.kt
+++ openmrs-client/src/test/java/edu/upc/sdk/library/api/repository/NewVisitRepositoryTest.kt
@@ -4,26 +4,36 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import edu.upc.blopup.model.BloodPressure
 import edu.upc.blopup.model.Visit
 import edu.upc.sdk.library.api.RestApi
+import edu.upc.sdk.library.dao.VisitDAO
+import edu.upc.sdk.library.models.Patient
 import edu.upc.sdk.library.models.VisitExample
 import io.mockk.MockKAnnotations
 import io.mockk.coEvery
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
 import io.mockk.mockk
+import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import retrofit2.Call
 import retrofit2.Response
+import rx.Observable
+import java.time.Instant
 import java.time.LocalDateTime
+import java.time.temporal.ChronoUnit.SECONDS
 import java.util.UUID
+import edu.upc.sdk.library.models.Visit as OpenMRSVisit
 
 @RunWith(AndroidJUnit4::class)
 class NewVisitRepositoryTest {
 
     @MockK
     private lateinit var restApi: RestApi
+
+    @MockK
+    private lateinit var visitDAO: VisitDAO
 
     @InjectMockKs
     private lateinit var visitRepository: NewVisitRepository
@@ -41,7 +51,7 @@ class NewVisitRepositoryTest {
             visitUuid,
             patientUuid,
             "La casa de Ale",
-            LocalDateTime.now().truncatedTo(java.time.temporal.ChronoUnit.SECONDS),
+            LocalDateTime.now().truncatedTo(SECONDS),
             BloodPressure(120, 80, 70),
             177,
             70.0f
@@ -60,12 +70,89 @@ class NewVisitRepositoryTest {
         )
         val response = Response.success(openMRSVisit)
 
-        val call = mockk<Call<edu.upc.sdk.library.models.Visit>>(relaxed = true)
+        val call = mockk<Call<OpenMRSVisit>>(relaxed = true)
         coEvery { restApi.getVisitByUuid(visitUuid.toString()) } returns call
         coEvery { call.execute() } returns response
 
         val result = visitRepository.getVisitByUuid(visitUuid)
 
         assertEquals(expectedVisit, result)
+    }
+
+    @Test
+    fun `should end visit`() {
+        val visitUuid = UUID.randomUUID()
+        val patientUuid = UUID.randomUUID()
+
+        val visitId = 123L
+        val patientId = 456L
+
+        val openMRSVisit = OpenMRSVisit().apply {
+            stopDatetime = Instant.now().toString()
+            uuid = visitUuid.toString()
+            id = visitId
+            patient = Patient().apply { uuid = patientUuid.toString(); id = patientId }
+        }
+
+        val response = Response.success(openMRSVisit)
+
+        val call = mockk<Call<OpenMRSVisit>>(relaxed = true)
+        coEvery { restApi.endVisitByUUID(visitUuid.toString(), any()) } returns call
+        coEvery { call.execute() } returns response
+
+        coEvery { visitDAO.getVisitsIDByUUID(visitUuid.toString()) } returns Observable.just(visitId)
+        coEvery { visitDAO.getVisitByID(visitId) } returns Observable.just(openMRSVisit)
+        coEvery { visitDAO.saveOrUpdate(openMRSVisit, patientId) } returns Observable.just(visitId)
+
+        runBlocking {
+            val result = visitRepository.endVisit(visitUuid)
+
+            assertEquals(result, true)
+        }
+    }
+
+    @Test(expected = Exception::class)
+    fun `should throw exception if ending visit fails`() {
+        val visitUuid = UUID.randomUUID()
+
+        val response = Response.error<OpenMRSVisit>(500, mockk())
+
+        val call = mockk<Call<OpenMRSVisit>>(relaxed = true)
+        coEvery { restApi.endVisitByUUID(visitUuid.toString(), any()) } returns call
+        coEvery { call.execute() } returns response
+
+        runBlocking {
+            visitRepository.endVisit(visitUuid)
+        }
+    }
+
+    @Test
+    fun `should log an error and continue if updating local database fails`() {
+        val visitUuid = UUID.randomUUID()
+        val patientUuid = UUID.randomUUID()
+
+        val visitId = 123L
+        val patientId = 456L
+
+        val openMRSVisit = OpenMRSVisit().apply {
+            stopDatetime = Instant.now().toString()
+            uuid = visitUuid.toString()
+            id = visitId
+            patient = Patient().apply { uuid = patientUuid.toString(); id = patientId }
+        }
+
+        val response = Response.success(openMRSVisit)
+
+        val call = mockk<Call<OpenMRSVisit>>(relaxed = true)
+        coEvery { restApi.endVisitByUUID(visitUuid.toString(), any()) } returns call
+        coEvery { call.execute() } returns response
+
+        coEvery { visitDAO.getVisitsIDByUUID(visitUuid.toString()) } throws Exception()
+
+        runBlocking {
+            val result = visitRepository.endVisit(visitUuid)
+
+            assertEquals(result, true)
+        }
     }
 }
