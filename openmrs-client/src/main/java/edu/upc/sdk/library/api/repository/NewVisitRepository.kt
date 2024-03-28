b05526bb74865d3b5c2113675fd2f93e3dafa1c6
rosapalli <rosa.palli@thoughtworks.com>
2024-03-28 10:08:51 +0100

    BLOPUP-385: extract method to convert OpenMRSVisit to Visit

--- openmrs-client/src/main/java/edu/upc/sdk/library/api/repository/NewVisitRepository.kt
+++ openmrs-client/src/main/java/edu/upc/sdk/library/api/repository/NewVisitRepository.kt
@@ -28,81 +28,45 @@ import edu.upc.sdk.library.models.Visit as OpenMRSVisit
 
 @Singleton
 class NewVisitRepository @Inject constructor(
-    val restApi: RestApi,
-    val visitDAO: VisitDAO,
-    private val locationDAO: LocationDAO
+    val restApi: RestApi, val visitDAO: VisitDAO, private val locationDAO: LocationDAO
 ) {
 
     private val logger = OpenMRSLogger()
 
     fun getVisitByUuid(visitUuid: UUID): Visit {
-
         val openMRSVisit = restApi.getVisitByUuid(visitUuid.toString()).execute().body()
-
-        val vitalsObservations =
-            openMRSVisit?.encounters?.find { it.encounterType?.display == EncounterType.VITALS }?.observations
-
-        val systolic =
-            vitalsObservations?.find { it.display?.contains("Systolic") == true }?.displayValue?.toDouble()
-                ?.toInt() ?: throw Exception("Systolic is null")
-        val diastolic =
-            vitalsObservations.find { it.display?.contains("Diastolic") == true }?.displayValue?.toDouble()
-                ?.toInt() ?: throw Exception("Diastolic is null")
-        val pulse =
-            vitalsObservations.find { it.display?.contains("Pulse") == true }?.displayValue?.toDouble()
-                ?.toInt() ?: throw Exception("Pulse is null")
-        val weight =
-            vitalsObservations.find { it.display?.contains("Weight") == true }?.displayValue?.toFloat()
-        val height =
-            vitalsObservations.find { it.display?.contains("Height") == true }?.displayValue?.toDouble()
-                ?.toInt()
-
-        val visitStartDate =
-            openMRSVisit.startDatetime.let { DateUtils.parseLocalDateFromOpenmrsDate(it) }
-
-        return Visit(
-            visitUuid,
-            UUID.fromString(openMRSVisit.patient.uuid),
-            openMRSVisit.location.display ?: "",
-            visitStartDate,
-            BloodPressure(systolic, diastolic, pulse),
-            height,
-            weight
-        )
+        return createVisitFromOpenMRSVisit(openMRSVisit)
     }
 
-    suspend fun endVisit(visitId: UUID): Boolean =
-        withContext(Dispatchers.IO) {
-            val endVisitDateTime =
-                LocalDateTime.now().truncatedTo(SECONDS).toInstant(ZoneOffset.UTC).toString()
-            val visitWithEndDate = OpenMRSVisit().apply {
-                stopDatetime = endVisitDateTime
-            }
-
-            val call: Call<OpenMRSVisit> =
-                restApi.endVisitByUUID(visitId.toString(), visitWithEndDate)
-            val response = call.execute()
-
-            if (response.isSuccessful) {
-                try {
-                    val localVisitId =
-                        visitDAO.getVisitsIDByUUID(visitId.toString()).single().toBlocking().first()
-                    val localVisit =
-                        visitDAO.getVisitByID(localVisitId).single().toBlocking().first()
-                    localVisit.patient.id?.let {
-                        localVisit.stopDatetime = endVisitDateTime
-                        visitDAO.saveOrUpdate(localVisit, it).single().toBlocking().first()
-                    }
-                } catch (e: Exception) {
-                    logger.e("Error updating visit end date in local database: ${e.javaClass.simpleName}: ${e.message}")
-                }
-                true
-            } else {
-                throw Exception("endVisitByUuid error: " + response.message())
-            }
+    suspend fun endVisit(visitId: UUID): Boolean = withContext(Dispatchers.IO) {
+        val endVisitDateTime =
+            LocalDateTime.now().truncatedTo(SECONDS).toInstant(ZoneOffset.UTC).toString()
+        val visitWithEndDate = OpenMRSVisit().apply {
+            stopDatetime = endVisitDateTime
         }
 
-    fun startVisit(patient: Patient): Visit? {
+        val call: Call<OpenMRSVisit> = restApi.endVisitByUUID(visitId.toString(), visitWithEndDate)
+        val response = call.execute()
+
+        if (response.isSuccessful) {
+            try {
+                val localVisitId =
+                    visitDAO.getVisitsIDByUUID(visitId.toString()).single().toBlocking().first()
+                val localVisit = visitDAO.getVisitByID(localVisitId).single().toBlocking().first()
+                localVisit.patient.id?.let {
+                    localVisit.stopDatetime = endVisitDateTime
+                    visitDAO.saveOrUpdate(localVisit, it).single().toBlocking().first()
+                }
+            } catch (e: Exception) {
+                logger.e("Error updating visit end date in local database: ${e.javaClass.simpleName}: ${e.message}")
+            }
+            true
+        } else {
+            throw Exception("endVisitByUuid error: " + response.message())
+        }
+    }
+
+    fun startVisit(patient: Patient): Visit {
         val openMRSVisit = OpenMRSVisit().apply {
             startDatetime = Instant.now().toString()
             this.patient = patient
@@ -114,50 +78,50 @@ class NewVisitRepository @Inject constructor(
         val response = call.execute()
         if (response.isSuccessful) {
             val newVisit = response.body()
-            val vitalsObservations =
-                newVisit?.encounters?.find { it.encounterType?.display == EncounterType.VITALS }?.observations
-            val bloodPressure =
-                vitalsObservations.let { vitals ->
-                    val systolic =
-                        vitals?.find { it.display?.contains("Systolic") == true }?.displayValue?.toDouble()
-                            ?.toInt() ?: throw Exception("Systolic is null")
-                    val diastolic =
-                        vitals.find { it.display?.contains("Diastolic") == true }?.displayValue?.toDouble()
-                            ?.toInt() ?: throw Exception("Diastolic is null")
-                    val pulse =
-                        vitals.find { it.display?.contains("Pulse") == true }?.displayValue?.toDouble()
-                            ?.toInt() ?: throw Exception("Pulse is null")
-                    BloodPressure(systolic, diastolic, pulse)
-                }
-
-            val height =
-                vitalsObservations?.find { it.display?.contains("Height") == true }?.displayValue?.toDouble()
-                    ?.toInt()
-
-            val weight =
-                vitalsObservations?.find { it.display?.contains("Weight") == true }?.displayValue?.toFloat()
-
-            val visit =
-                newVisit?.startDatetime?.let { DateUtils.parseLocalDateFromOpenmrsDate(it) }
-                    ?.let { localDateTime ->
-                        Visit(
-                            UUID.fromString(newVisit.uuid),
-                            UUID.fromString(newVisit.patient.uuid),
-                            newVisit.location.display ?: "",
-                            localDateTime,
-                            bloodPressure,
-                            height,
-                            weight
-                        )
-                    }
             try {
                 visitDAO.saveOrUpdate(newVisit, patient.id!!).toBlocking()
             } catch (e: Exception) {
                 logger.e("Error saving new visit in local database: ${e.javaClass.simpleName}: ${e.message}")
             }
-            return visit
+            return createVisitFromOpenMRSVisit(newVisit)
         } else {
             throw IOException(response.message())
         }
+    }
+
+    private fun createVisitFromOpenMRSVisit(openMRSVisit: OpenMRSVisit?): Visit {
+        val vitalsObservations =
+            openMRSVisit?.encounters?.find { it.encounterType?.display == EncounterType.VITALS }?.observations
+
+        val bloodPressure = vitalsObservations.let { vitals ->
+            val systolic =
+                vitals?.find { it.display?.contains("Systolic") == true }?.displayValue?.toDouble()
+                    ?.toInt() ?: throw Exception("Systolic is null")
+            val diastolic =
+                vitals.find { it.display?.contains("Diastolic") == true }?.displayValue?.toDouble()
+                    ?.toInt() ?: throw Exception("Diastolic is null")
+            val pulse =
+                vitals.find { it.display?.contains("Pulse") == true }?.displayValue?.toDouble()
+                    ?.toInt() ?: throw Exception("Pulse is null")
+            BloodPressure(systolic, diastolic, pulse)
+        }
+
+        val height =
+            vitalsObservations?.find { it.display?.contains("Height") == true }?.displayValue?.toDouble()
+                ?.toInt()
+
+        val weight =
+            vitalsObservations?.find { it.display?.contains("Weight") == true }?.displayValue?.toFloat()
+
+        return Visit(
+            UUID.fromString(openMRSVisit?.uuid),
+            UUID.fromString(openMRSVisit?.patient?.uuid),
+            openMRSVisit?.location?.display ?: "",
+            openMRSVisit?.startDatetime?.let { DateUtils.parseLocalDateFromOpenmrsDate(it) }
+                ?: LocalDateTime.now(),
+            bloodPressure,
+            height,
+            weight
+        )
     }
 }
