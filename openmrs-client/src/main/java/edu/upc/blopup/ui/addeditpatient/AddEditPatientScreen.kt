a3e3d6664f918e6f5a5b35dffc711bac8a688a2e
Carolina Acosta <carolina.acosta@thoughtworks.com>
2024-04-04 14:09:42 -0300

    #433 - Add gender radio buttons and replace paddings per space between.

--- openmrs-client/src/main/java/edu/upc/blopup/ui/addeditpatient/AddEditPatientScreen.kt
+++ openmrs-client/src/main/java/edu/upc/blopup/ui/addeditpatient/AddEditPatientScreen.kt
@@ -1,11 +1,15 @@
 package edu.upc.blopup.ui.addeditpatient
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material3.Button
@@ -15,6 +19,8 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.RadioButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
@@ -22,6 +28,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
@@ -53,40 +60,63 @@ fun AddEditPatientForm() {
     val datePickerState = rememberDatePickerState()
     var showDatePickerDialog by remember { mutableStateOf(false) }
     var estimatedYears by remember { mutableStateOf("") }
-
-    val outlinedTextFieldColors = OutlinedTextFieldDefaults.colors(
-        focusedBorderColor = colorResource(R.color.primary),
-        focusedLabelColor = colorResource(R.color.primary),
-        cursorColor = Color.Black,
-        errorLabelColor = Color.Red,
-        errorContainerColor = Color.Red)
+    var gender by remember { mutableStateOf("") }
 
     Column(
         Modifier
             .fillMaxSize()
-            .padding(16.dp)
+            .padding(25.dp)
+            .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.SpaceBetween
     ) {
-        Column(Modifier.padding(vertical = 20.dp)) {
+        Column {
             StructureLabelText(R.string.reg_ques_name)
             OutlinedTextField(
                 modifier = Modifier.fillMaxWidth(),
                 value = name,
                 onValueChange = { name = it },
                 label = { Text(text = stringResource(R.string.reg_firstname_hint)) },
-                colors = outlinedTextFieldColors
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedBorderColor = colorResource(R.color.primary),
+                    focusedLabelColor = colorResource(R.color.primary),
+                    cursorColor = Color.Black),
+                isError = name.isEmpty()
             )
             OutlinedTextField(
                 modifier = Modifier.fillMaxWidth(),
                 value = familyName,
                 onValueChange = { familyName = it },
                 label = { Text(text = stringResource(R.string.reg_surname_hint)) },
-                colors = outlinedTextFieldColors
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedBorderColor = colorResource(R.color.primary),
+                    focusedLabelColor = colorResource(R.color.primary),
+                    cursorColor = Color.Black),
+                isError = familyName.isEmpty()
+
             )
         }
-        Column(Modifier.padding(vertical = 20.dp)) {
+        Column {
             StructureLabelText(R.string.reg_ques_gender)
+            if(gender.isEmpty()) {
+                Text(
+                    stringResource(R.string.choose_option_error),
+                    color = colorResource(R.color.error_red)
+                )
+            }
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                RadioButton(selected = gender == "M" , onClick = { gender = "M"}, colors = RadioButtonDefaults.colors(colorResource(R.color.allergy_orange)))
+                Text(text = stringResource(R.string.male), fontSize = TextUnit(16f, TextUnitType.Sp))
+            }
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                RadioButton(selected = gender == "F", onClick = { gender = "F"}, colors = RadioButtonDefaults.colors(colorResource(R.color.allergy_orange)))
+                Text(text = stringResource(R.string.female), fontSize = TextUnit(16f, TextUnitType.Sp))
+            }
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                RadioButton(selected = gender == "N", onClick = { gender = "N"}, colors = RadioButtonDefaults.colors(colorResource(R.color.allergy_orange)))
+                Text(text = stringResource(R.string.non_binary), fontSize = TextUnit(16f, TextUnitType.Sp))
+            }
         }
-        Column(Modifier.padding(vertical = 20.dp)) {
+        Column {
             val date = datePickerState.selectedDateMillis
             if(date != null) {
                 val localDate = Instant.ofEpochMilli(date).atZone(ZoneId.of("UTC")).toLocalDate()
@@ -97,8 +127,12 @@ fun AddEditPatientForm() {
                 modifier = Modifier.fillMaxWidth(),
                 value = dateOfBirth,
                 onValueChange = { dateOfBirth = it },
+                isError = dateOfBirth.isEmpty() && estimatedYears.isEmpty(),
                 label = { Text(text = stringResource(R.string.dob_hint),  modifier = Modifier.clickable(onClick = { showDatePickerDialog = true })) },
-                colors = outlinedTextFieldColors,
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedBorderColor = colorResource(R.color.primary),
+                    focusedLabelColor = colorResource(R.color.primary),
+                    cursorColor = Color.Black),
                 keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
                 trailingIcon = {
                     Icon(
@@ -114,9 +148,13 @@ fun AddEditPatientForm() {
             OutlinedTextField(
                 modifier = Modifier.fillMaxWidth(),
                 value = estimatedYears,
+                isError = dateOfBirth.isEmpty() && estimatedYears.isEmpty(),
                 onValueChange = { estimatedYears = it },
                 label = { Text(text = stringResource(R.string.estyr), fontSize = TextUnit(16f, TextUnitType.Sp)) },
-                colors = outlinedTextFieldColors,
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedBorderColor = colorResource(R.color.primary),
+                    focusedLabelColor = colorResource(R.color.primary),
+                    cursorColor = Color.Black),
                 keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number))
             if (showDatePickerDialog) {
                 DatePickerDialog(onDismissRequest = { showDatePickerDialog = false },
@@ -129,10 +167,12 @@ fun AddEditPatientForm() {
                 }
             }
         }
-        Column(Modifier.padding(vertical = 20.dp)) {
+        Column {
             StructureLabelText(R.string.record_patient_consent)
         }
-        SubmitButton(title = R.string.action_submit, onClickNext = { }, enabled = false )
+        Column {
+            SubmitButton(title = R.string.action_submit, onClickNext = { }, enabled = false )
+        }
     }
 }
 
