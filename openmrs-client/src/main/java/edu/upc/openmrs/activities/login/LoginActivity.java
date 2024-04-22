e8c68b0811344c9bc70354ea4d3a3d576db311c6
Alejandro Batanero <alejandro.batanero@thoughtworks.com>
2024-04-22 18:14:35 +0200

    BLOPUP-429 Remove not used graph navigation

--- openmrs-client/src/main/java/edu/upc/openmrs/activities/login/LoginActivity.java
+++ openmrs-client/src/main/java/edu/upc/openmrs/activities/login/LoginActivity.java
@@ -60,6 +60,7 @@ public class LoginActivity extends ACBaseActivity {
 
     @Override
     public void onBackPressed() {
+        super.onBackPressed();
         Intent intent = new Intent(Intent.ACTION_MAIN);
         intent.addCategory(Intent.CATEGORY_HOME);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
