c7365fa100f19c2af6d39af9dd4cb457a0f347e8
Alejandro Batanero <alejandro.batanero@thoughtworks.com>
2024-04-22 16:52:59 +0200

    Remove multidex library

--- openmrs-client/src/main/java/edu/upc/openmrs/application/OpenMRS.java
+++ openmrs-client/src/main/java/edu/upc/openmrs/application/OpenMRS.java
@@ -15,27 +15,19 @@
 package edu.upc.openmrs.application;
 
 import android.app.Activity;
+import android.app.Application;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
-
-import androidx.multidex.MultiDexApplication;
-
-import java.io.File;
 
 import dagger.hilt.android.HiltAndroidApp;
 import edu.upc.blopup.hilt.CurrentActivityProvider;
 import edu.upc.openmrs.services.AuthenticateCheckService;
-import edu.upc.sdk.library.OpenMRSLogger;
 import edu.upc.sdk.library.OpenmrsAndroid;
 
 @HiltAndroidApp
-public class OpenMRS extends MultiDexApplication {
-    private static final String OPENMRS_DIR_NAME = "OpenMRS";
-    private static final String OPENMRS_DIR_PATH = File.separator + OPENMRS_DIR_NAME;
+public class OpenMRS extends Application {
     private static String mExternalDirectoryPath;
     private static OpenMRS instance;
-    private OpenMRSLogger mLogger;
     private final MBCActivityLifecycleCallbacks mCallbacks = new MBCActivityLifecycleCallbacks();
 
     public static OpenMRS getInstance() {
@@ -54,7 +46,6 @@ public class OpenMRS extends MultiDexApplication {
         if (mExternalDirectoryPath == null) {
             mExternalDirectoryPath = this.getExternalFilesDir(null).toString();
         }
-        mLogger = new OpenMRSLogger();
         Intent intent = new Intent(this, AuthenticateCheckService.class);
         startService(intent);
     }
@@ -62,10 +53,6 @@ public class OpenMRS extends MultiDexApplication {
     @Override
     public void onTerminate() {
         super.onTerminate();
-    }
-
-    public boolean isRunningKitKatVersionOrHigher() {
-        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
     }
 
     public static class MBCActivityLifecycleCallbacks implements ActivityLifecycleCallbacks {
