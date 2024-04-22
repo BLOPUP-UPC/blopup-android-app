aef68d195be3d9f08618b190348f1eb66db0a330
Alejandro Batanero <alejandro.batanero@thoughtworks.com>
2024-04-22 12:27:32 +0200

    BLOPUP-443 Remove complex server url regex check

--- openmrs-client/src/test/java/edu/upc/openmrs/utilities/URLValidatorTest.java
+++ openmrs-client/src/test/java/edu/upc/openmrs/utilities/URLValidatorTest.java
@@ -20,19 +20,19 @@ import org.junit.Test;
 
 public class URLValidatorTest {
     private static final String INVALID_URL_1;
-    private static final String INVALID_URL_2;
     private static final String VALID_URL_1;
     private static final String VALID_URL_1_TRIMMED;
     private static final String VALID_URL_2;
     private static final String VALID_URL_3;
+    private static final String VALID_URL_4;
 
     static {
         INVALID_URL_1 = "http://";
-        INVALID_URL_2 = "http://demo.openmrs.org/openmrsl.na15.force.com";
         VALID_URL_1 = "http://demo.openmrs.org/openmrs/";
         VALID_URL_1_TRIMMED = "http://demo.openmrs.org/openmrs";
         VALID_URL_2 = "https://demo.openmrs.org:8081/openmrs-standalone";
         VALID_URL_3 = "http://demo.openmrs.org/openmrs/ ";
+        VALID_URL_4 = "http://10.0.2.2/openmrs";
     }
 
     @Test
@@ -44,12 +44,6 @@ public class URLValidatorTest {
         expected = new URLValidator.ValidationResult(false, INVALID_URL_1);
         assertEquals(expected.isURLValid(), result.isURLValid());
         assertEquals(expected.getUrl(), result.getUrl());
-
-        result = URLValidator.validate(INVALID_URL_2);
-        expected = new URLValidator.ValidationResult(false, INVALID_URL_2);
-        assertEquals(expected.isURLValid(), result.isURLValid());
-        assertEquals(expected.getUrl(), result.getUrl());
-
 
         result = URLValidator.validate(VALID_URL_1);
         expected = new URLValidator.ValidationResult(true, VALID_URL_1_TRIMMED);
@@ -63,6 +57,11 @@ public class URLValidatorTest {
 
         result = URLValidator.validate(VALID_URL_3);
         expected = new URLValidator.ValidationResult(true, VALID_URL_1_TRIMMED);
+        assertEquals(expected.isURLValid(), result.isURLValid());
+        assertEquals(expected.getUrl(), result.getUrl());
+
+        result = URLValidator.validate(VALID_URL_4);
+        expected = new URLValidator.ValidationResult(true, VALID_URL_4);
         assertEquals(expected.isURLValid(), result.isURLValid());
         assertEquals(expected.getUrl(), result.getUrl());
     }
