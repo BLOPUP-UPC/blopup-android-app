5ca550440d9ff684a07484712d3fb90e3093b6c8
Carolina Acosta <carolina.acosta@thoughtworks.com>
2024-04-25 16:26:49 +0200

    BLOPUP-435- Add Focus to the search input when clicking the search icon.

--- openmrs-client/src/main/java/edu/upc/blopup/ui/shared/components/SearchInput.kt
+++ openmrs-client/src/main/java/edu/upc/blopup/ui/shared/components/SearchInput.kt
@@ -7,14 +7,26 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.sp
 import edu.upc.R
 
 @Composable
-fun SearchInput(searchInput: String, onSearchQueryChange: (String) -> Unit) {
+fun SearchInput(
+    searchInput: String,
+    onSearchQueryChange: (String) -> Unit,
+) {
+    val focusRequester = remember { FocusRequester() }
+
     TextField(
+        modifier = Modifier.focusRequester(focusRequester),
         value = searchInput, onValueChange = { onSearchQueryChange(it) },
         colors = TextFieldDefaults.colors(
             focusedPlaceholderColor = Color.LightGray,
@@ -26,8 +38,11 @@ fun SearchInput(searchInput: String, onSearchQueryChange: (String) -> Unit) {
             focusedContainerColor = colorResource(R.color.dark_teal),
             unfocusedContainerColor = colorResource(R.color.dark_teal),
         ),
-        placeholder = { Text(stringResource(R.string.search_hint_text)) }
+        placeholder = { Text(text = stringResource(R.string.search_hint_text), fontSize = 24.sp ) }
     )
+    LaunchedEffect(true) {
+        focusRequester.requestFocus()
+    }
 }
 
 
